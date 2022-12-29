bf05cfb8ce5d8ed7f7ba220a5dd5c247c3537f87
setter11 <setter11@localhost.localdomain>
2022-12-29 13:40:55 +0700

    offline page

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,11 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'KelurahanBedoyo';
 // Customize this with a different URL if needed.
+const OFFLINE_URL = './offline.html';
 var urlsToCache = [
+  'offline.html',
   '/',
   '/dasbor',
   '/profil',
@@ -28,10 +17,6 @@ var urlsToCache = [
   '/kematian',
   '/pindah',
   '/lahir',
-  '/link1',
-  '/link2',
-  '/link3',
-  '/link4',
   '/datapegawai',
   '/manifest.json',
   '/favicon.ico',
@@ -92,6 +77,10 @@ self.addEventListener('fetch', function(event) {
         }
         return fetch(event.request);
       }
-    )
+    ).catch(async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(OFFLINE_URL);
+      return cachedResponse;
+    })
   );
 });
